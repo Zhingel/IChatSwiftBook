eaf3bfba23c0920dcc2d3e37909835eb6f0ff252
Stas <staszhingel@icloud.com>
2021-08-22 19:16:38 +0300

    Initial Commit

---
+++ IChatSwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IChatSwiftBook
+//
+//  Created by Стас Жингель on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
